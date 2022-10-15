3e8a29beff3ab0c2b7d1c41b809681f42be5c603
whonehir <nehirakgul001@gmail.com>
2022-10-15 23:20:48 +0300

    python

---
+++ ortalama.py
@@ -0,0 +1,6 @@
+a = float(input("Bir sayı girin: "))
+b = float(input("Bir sayı girin: "))
+c = float(input("Bir sayı girin: "))
+d = float(input("Bir sayı girin: "))
+
+print("Ortalama:", (a+b+c+d)/4)
